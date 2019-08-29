3908104246902a1f52f82c515949fc4e5319db20
Sergey Lvov <log0div0@gmail.com>
2019-08-29 19:51:04 +0300

    rm calendar & statistics

--- src/pages/dashboards/Default/index.js
+++ src/pages/dashboards/Default/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 
-import Statistics from "./Statistics";
 import LineChart from "./LineChart";
-import Calendar from "./Calendar";
 import PieChart from "./PieChart";
 import Appointments from "./Appointments";
 import Projects from "./Projects";
