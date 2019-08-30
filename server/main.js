dc781cd2895ad265745024717083ac589e3c3963
Sergey Lvov <log0div0@gmail.com>
2019-08-30 18:34:28 +0300

    render index page

--- server/main.js
+++ server/main.js
@@ -1,8 +1,29 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
-app.use(express.static('public'))
+const app = express();
+const port = 3000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+const page =
+	<html lang="ru">
+	  <head>
+	    <meta charSet="utf-8"/>
+	    <title>Testo Dashboard</title>
+	    <link rel="preconnect" href="//fonts.gstatic.com/" crossOrigin=""/>
+	  </head>
+	  <body>
+	    <div id="root"></div>
+	  </body>
+	    <script src="dist/main.js"></script>
+	</html>
+;
+
+app.get('/', (req, res) => {
+	res.send('<!DOCTYPE html>' + ReactDOMServer.renderToStaticMarkup(page));
+});
+app.use(express.static('public'));
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
