044623b13f478f3227657d3f606eed11fd9ab844
Sergey Lvov <log0div0@gmail.com>
2019-08-31 23:27:54 +0300

    rm App.js

--- client/index.js
+++ client/index.js
@@ -4,6 +4,15 @@ import './styles/_app.scss'
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import store from "./store";
+
+import { Provider } from "react-redux";
+import Dashboard from "./pages/dashboard";
+
+const App = () => (
+	<Provider store={store}>
+		<Dashboard/>
+	</Provider>
+);
 
 ReactDOM.render(<App />, document.getElementById("root"));
